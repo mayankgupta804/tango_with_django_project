4fb48fe5ef7ba07815d3971f9b56f5557dc8c59c
mayank <mayank.gupta804@gmail.com>
2018-07-24 00:20:22 +0530

    Add static files + adding views for about and index pages

--- rango/views.py
+++ rango/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    html = '''
-    <a href='/rango/about'>About</a>
-    <p>Rango says hello world</p>
-    '''
-    return HttpResponse(html)
+    context_dict={'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    html = '''
-    <a href='/rango/'>Home</a>
-    <p>Rango says here is the about page</p>
-    '''
-    return HttpResponse(html)    
+    context_dict={'yourname':"Mayank"}
+    return render(request, 'rango/about.html', context=context_dict)   
